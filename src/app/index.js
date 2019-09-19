9c9885fbbce02fef74e6d7379a1a4ac269214f5f
mpropc2 <hongocman0501@gmail.com>
2019-09-19 22:21:28 +0700

    created scrollable list

--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,8 @@ for (let i = 0; i < 10000; i++) {
     elements[i] = i;
 }
 
-const menuDropdown = document.getElementById("menu-dropdown");
+const container = document.getElementsByClassName("menu-container")[0];
+const menuDropdown = document.getElementById('menu-dropdown');
 for (let i = 0; i < 15; i++) {
     menuDropdown.insertAdjacentHTML('beforeend', `<div class="element">element${i + 1}</div>`);
 }
@@ -14,16 +15,6 @@ let initialScrollTop = 0;
 let lastScrollTop = 0;
 const visibleItem = 10; 
 function print(e) {
-    let liTags = e.target.childNodes;
-    liTags.forEach((element, index) => {
-        let menuDropdownRect = menuDropdown.getBoundingClientRect();
-        if (index != 0) {
-            var elementRect = element.getBoundingClientRect();
-            if (elementRect.top + elementRect.height > 0 && elementRect.top < menuDropdownRect.height ) {
-                // console.log(element);
-            }
-        }
-    });
     console.log("aa ", e.target.scrollTop);
     var st = e.target.scrollTop;
     var distance = st - lastScrollTop;
@@ -39,4 +30,4 @@ function print(e) {
     lastScrollTop = st <= 0 ? 0 : st;
 }
 
-menuDropdown.onscroll = print;
+container.onscroll = print;
